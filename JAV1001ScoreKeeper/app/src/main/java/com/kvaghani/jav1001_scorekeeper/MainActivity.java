f2dfedbe9988e3a037628ef4685fe7021242998c
kushal <kushal_vaghani@yahoo.com>
2022-02-28 04:28:36 -0500

    seekbar implementation

--- JAV1001ScoreKeeper/app/src/main/java/com/kvaghani/jav1001_scorekeeper/MainActivity.java
+++ JAV1001ScoreKeeper/app/src/main/java/com/kvaghani/jav1001_scorekeeper/MainActivity.java
@@ -3,6 +3,7 @@ package com.kvaghani.jav1001_scorekeeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.renderscript.Sampler;
 import android.view.View;
 import android.widget.*;
 
@@ -11,9 +12,10 @@ public class MainActivity extends AppCompatActivity {
 
     TextView team1Score;
     TextView team2Score;
+    TextView ScoreIncrease;
 
     //Button team1AddButton;
-
+    SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
         team1Score = findViewById(R.id.team1Score);
         team2Score = findViewById(R.id.team2Score);
+        ScoreIncrease = findViewById(R.id.ScoreIncreaseLbl);
 
         //setting up the four buttons ± 
         Button team1Add = (Button) findViewById(R.id.team1AddButton);
@@ -41,7 +44,10 @@ public class MainActivity extends AppCompatActivity {
         Button team1Subtract = (Button) findViewById(R.id.team1SubtractButton);
         team1Subtract.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                team1Score.setText("button - was pressed");  // Do something in response to button click
+        //      int tempScore = ScoreIncrease.getText();
+//               int oldScore = Integer.parseInt(team1Score.getText());
+             //   int updatedScore = (oldScore - tempScore);
+              //  team1Score.setText("Score: " + updatedScore);  // Do something in response to button click
             }
         });
 
@@ -51,8 +57,24 @@ public class MainActivity extends AppCompatActivity {
                 team2Score.setText("button - was pressed");  // Do something in response to button click
             }
         });
+        seekBar = (SeekBar)findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
 
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                ScoreIncrease.setText(String.valueOf(i));
+                System.out.println(i);
+            }
 
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
 
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
