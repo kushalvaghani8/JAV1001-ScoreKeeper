28a3696c003643770eba2e10df4df916206fea6d
kushal <kushal_vaghani@yahoo.com>
2022-02-28 14:35:31 -0500

    Final commit
    reduced code redundancy by implementing methods, added a textview for error message and added condition for result - cannot be in negative

--- JAV1001ScoreKeeper/app/src/main/java/com/kvaghani/jav1001_scorekeeper/MainActivity.java
+++ JAV1001ScoreKeeper/app/src/main/java/com/kvaghani/jav1001_scorekeeper/MainActivity.java
@@ -3,7 +3,6 @@ package com.kvaghani.jav1001_scorekeeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.renderscript.Sampler;
 import android.view.View;
 import android.widget.*;
 
@@ -13,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     TextView team1Score;
     TextView team2Score;
     TextView ScoreIncrease;
+    TextView errorMessage;
 
     //Button team1AddButton;
     SeekBar seekBar;
@@ -25,38 +25,95 @@ public class MainActivity extends AppCompatActivity {
         team1Score = findViewById(R.id.team1Score);
         team2Score = findViewById(R.id.team2Score);
         ScoreIncrease = findViewById(R.id.ScoreIncreaseLbl);
+        errorMessage = findViewById(R.id.textErrorMessage);
 
-        //setting up the four buttons ± 
+        //setting up the four buttons ±
         Button team1Add = (Button) findViewById(R.id.team1AddButton);
         team1Add.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                team1Score.setText("button + was pressed");  // Do something in response to button click
+                errorMessage.setText(""); //clearing the error message
+
+                /* ------------------------------------------------
+                      CALLING ADD METHOD (PASSING IN TWO VALUES)
+                  -------------------------------------------------
+                */
+
+                int result = addMethod(Integer.parseInt(team1Score.getText().toString()), Integer.parseInt(ScoreIncrease.getText().toString()));
+                team1Score.setText(String.valueOf(result));
+
             }
         });
 
         Button team2Add = (Button) findViewById(R.id.team2addbutton);
         team2Add.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                team2Score.setText("button + was pressed");  // Do something in response to button click
+                errorMessage.setText("");
+               /* --------------------------------------------------
+                      CALLING ADD METHOD (PASSING IN TWO VALUES)
+                  --------------------------------------------------
+                */
+                int result = addMethod(Integer.parseInt(team2Score.getText().toString()), Integer.parseInt(ScoreIncrease.getText().toString()));
+                team2Score.setText(String.valueOf(result));
             }
         });
 
         Button team1Subtract = (Button) findViewById(R.id.team1SubtractButton);
         team1Subtract.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-        //      int tempScore = ScoreIncrease.getText();
-//               int oldScore = Integer.parseInt(team1Score.getText());
-             //   int updatedScore = (oldScore - tempScore);
-              //  team1Score.setText("Score: " + updatedScore);  // Do something in response to button click
+
+                /* -------------------------------------------------
+                    CALLING SUBTRACT METHOD (PASSING IN TWO VALUES)
+                  --------------------------------------------------
+                */
+                int result = subtractMethod( Integer.parseInt(team1Score.getText().toString()), Integer.parseInt(ScoreIncrease.getText().toString()));
+
+                /* -----------------------------------------------------------------
+                    CHECKING IF RESULT IS LESS THAN 0, ERROR MESSAGE WILL BE SHOWN
+                  ------------------------------------------------------------------
+                */
+
+                if (result < 0) {
+                    errorMessage.setText("Value cannot be negative");
+                }
+                else if (result >= 0) {
+                    errorMessage.setText("");//clearing the error message when not needed
+                    team1Score.setText(String.valueOf(result));
+                }
+
             }
         });
 
         Button team2SubtractButton = (Button) findViewById(R.id.team2SubtractButton);
         team2SubtractButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                team2Score.setText("button - was pressed");  // Do something in response to button click
+
+                  /* -------------------------------------------------
+                       CALLING SUBTRACT METHOD (PASSING IN TWO VALUES)
+                     --------------------------------------------------
+                    */
+                int result = subtractMethod( Integer.parseInt(team2Score.getText().toString()), Integer.parseInt(ScoreIncrease.getText().toString()));
+
+                /* -----------------------------------------------------------------
+                    CHECKING IF RESULT IS LESS THAN 0, ERROR MESSAGE WILL BE SHOWN
+                  ------------------------------------------------------------------
+                */
+                if (result < 0) {
+                    errorMessage.setText("Value cannot be negative");
+                }
+                else if (result >= 0) {
+                    errorMessage.setText(""); //clearing the error message when not needed
+                    team2Score.setText(String.valueOf(result));
+                }
             }
         });
+
+
+
+
+                /* -------------------------------------------------------------------
+                   SEEKBAR WITH A MAX VALUE OF 5 (5 IS SET IN XML - android:max="5")
+                  --------------------------------------------------------------------
+                */
         seekBar = (SeekBar)findViewById(R.id.seekBar);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
 
@@ -76,5 +133,16 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+    }
+    //subtract method that takes two numbers and returns result
+    private int subtractMethod(int oldNumber, int newNumber){
+        int result = oldNumber - newNumber;
+        return result;
+    }
+    //add method that takes two numbers and returns result
+    private int addMethod(int oldNumber, int newNumber){
+        int result = oldNumber + newNumber;
+        return result;
     }
 }
